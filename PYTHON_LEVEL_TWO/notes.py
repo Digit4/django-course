170d9919a724920fec9bf1d4e886d1a55072500b
Digit4 <dpantojee775@gmail.com>
2018-12-17 19:18:47 +0530

    completed regex

--- PYTHON_LEVEL_TWO/notes.py
+++ PYTHON_LEVEL_TWO/notes.py
@@ -16,7 +16,7 @@ text = "this is a string with term1 but not the  other one"
 
 for pat in patterns:
 	print("im searching for %s" % pat)
-	
+
 	if (re.search(pat, text)):
 		print("match")
 	else:
@@ -38,4 +38,21 @@ print(re.split(splitterm, email))
 
 # find all instances of a pattern
 
-print(re.findall('is','this is a string for it is a is'))
+print(re.findall('is', 'this is a string for it is a is'))
+
+# meta char find
+
+def multi_find(patterns, phrase):
+	for pat in patterns:
+		print("Searching for pattern {}".format(pat))
+		print(re.findall(pat, phrase))
+		print("\n")
+
+test = "This is a test string with numbers and stuff 129 40  symbol #$!@#"
+
+testpat = [r"\W+"]  #normal re works similar to re from TOC but for special RE, we can use escape symbols like
+				#r'\d' = all the numbers r'\D' = all non numeric values
+				#r'\s' sequence of whitespace r'\S' non whitespace
+				#r'\w' all alphanumeric r'\W' all non alphanumeric
+
+multi_find(testpat,test)
