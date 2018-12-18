4fa86b3f2a2b2cf12f1fb5627ff22a4d95102cd4
Digit4 <dpantojee775@gmail.com>
2018-12-18 10:24:55 +0530

    Completed template challange Django level one part 7.

--- DJANGO_LEVEL_ONE/pTwo/appTwo/views.py
+++ DJANGO_LEVEL_ONE/pTwo/appTwo/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(requests):
-	return HttpResponse("<h1><strong>My Second App</strong></h1>")
+def help(requests):
+	help_d = {"print_here": "<h1 align=\"center\">Help Page</h1>"}
+	return render(requests, "appTwo/help.html", context=help_d)
+
+def index(request):
+	return HttpResponse("<h1 clign='center'>My Second App</h1>")
--- DJANGO_LEVEL_ONE/pTwo/pTwo/urls.py
+++ DJANGO_LEVEL_ONE/pTwo/pTwo/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path,include
 from appTwo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="Second App is Here"),
+    url(r'^$',views.index,name = "Second App is Here"),
+    url(r'^help/', include("appTwo.urls")),
 ]
