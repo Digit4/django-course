ab499eca6af7cf3378a5ef29fd6ba61449cd6d54
Digit4 <dpantojee775@gmail.com>
2018-12-18 09:33:55 +0530

    Added the index.html template into hello_world_app folder for proper segregation.
    useful for making the codebase modular.

--- DJANGO_LEVEL_ONE/first_project/hello_world_app/views.py
+++ DJANGO_LEVEL_ONE/first_project/hello_world_app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	my_dict = {'insert_me': "Hello I'm from views.py!"}
-	return render(request,'index.html',context=my_dict)
+	return render(request,'hello_world_app/index.html',context=my_dict)
