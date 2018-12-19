d9e9fe1ffa32382f057329cb869b35044aaafb15
Dhaval Pantojee <dpantojee775@gmail.com>
2018-12-19 12:01:02 +0530

    applied validations.

--- DJANGO_LEVEL_TWO/basicforms/basicapp/forms.py
+++ DJANGO_LEVEL_TWO/basicforms/basicapp/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from django.core import validators
 
+def checkz(value):
+	if (value[0].lower() != 'z'):
+		raise forms.ValidationError("Name has to start with z")
 
 class NewForm(forms.Form):
-	name = forms.CharField()
+	name = forms.CharField(validators=[checkz])
 	email = forms.EmailField()
 	text = forms.CharField(widget=forms.Textarea)
+	botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
+
+
