9e771e1ac30348f05157c004a56ecbdd46aadcd2
Dhaval Pantojee <dpantojee775@gmail.com>
2018-12-19 11:35:36 +0530

    applied forms to the project.

--- DJANGO_LEVEL_TWO/basicforms/basicapp/views.py
+++ DJANGO_LEVEL_TWO/basicforms/basicapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from basicapp import forms
 
 # Create your views here.
+def index_view(request):
+	return render(request, 'basicapp/index.html')
+
+def form_view(request):
+	form = forms.NewForm()
+	if request.method == "POST":
+		form = forms.NewForm(request.POST)
+
+		if form.is_valid():
+			# Do something
+			print("Form Validation Success. Printing entered values in console")
+			print("Name:%s" % form.cleaned_data['name'])
+			print("Email:%s" % form.cleaned_data['email'])
+			print("Text:%s" % form.cleaned_data['text'])
+	return render(request, 'basicapp/form_page.html',{'form':form})
--- DJANGO_LEVEL_TWO/basicforms/basicforms/urls.py
+++ DJANGO_LEVEL_TWO/basicforms/basicforms/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from basicapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.index_view, name = "home"),
+    url(r'^form_page', views.form_view, name = "forms"),
 ]
