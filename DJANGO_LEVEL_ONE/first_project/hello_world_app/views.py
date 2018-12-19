725a37dac06fe434470f9db91978452664d4c50f
Dhaval Pantojee <dpantojee775@gmail.com>
2018-12-19 10:35:08 +0530

    arbitrary changes.

--- DJANGO_LEVEL_ONE/first_project/hello_world_app/views.py
+++ DJANGO_LEVEL_ONE/first_project/hello_world_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from hello_world_app.models import Topic, AccessRecord, WebPage
 
 # Create your views here.
+
+
 def index(request):
-	my_dict = {'insert_me': "Hello I'm from views.py!"}
-	return render(request,'hello_world_app/index.html',context=my_dict)
+    webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_record':webpage_list}
+    return render(request, 'hello_world_app/index.html', context=date_dict)
