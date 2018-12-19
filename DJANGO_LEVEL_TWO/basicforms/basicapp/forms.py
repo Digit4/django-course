5ad1f1d65580229452fffe105879d231a01fa8fa
Dhaval Pantojee <dpantojee775@gmail.com>
2018-12-19 12:38:19 +0530

    added proper validations.

--- DJANGO_LEVEL_TWO/basicforms/basicapp/forms.py
+++ DJANGO_LEVEL_TWO/basicforms/basicapp/forms.py
@@ -8,7 +8,13 @@ def checkz(value):
 class NewForm(forms.Form):
 	name = forms.CharField(validators=[checkz])
 	email = forms.EmailField()
+	verify_email = forms.EmailField(label='Enter your email again')
 	text = forms.CharField(widget=forms.Textarea)
 	botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
 
-
+	def clean(self):
+		all_clean_data = super().clean()
+		email = all_clean_data['email']
+		v_mail = all_clean_data["verify_email"]
+		if (email != v_mail):
+			raise forms.ValidationError("emails do not match")
